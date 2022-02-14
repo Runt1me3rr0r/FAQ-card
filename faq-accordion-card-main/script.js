6abbd1d023a9e8d555c2f3e99bf419d90575eccf
matthew-clay <alexandermike1999@gmail.com>
2022-02-14 13:32:26 +0630

    ready to publish

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,1 +1,36 @@
-console.log("FrontEnd Mentor");
+let btnAccordionTag = document.querySelectorAll(".btn-accordionQ");
+
+btnAccordionTag.forEach((el) => {
+  el.addEventListener("click", (event) => {
+    let toggleCollapseTag = el.nextElementSibling;
+
+    if (!el.classList.contains("open")) {
+      toggleCollapseTag.style.display = "block";
+      let toggleCollapseHeight = toggleCollapseTag.clientHeight;
+
+      setTimeout(() => {
+        toggleCollapseTag.style.height = toggleCollapseHeight + "px";
+        toggleCollapseTag.style.display = "";
+      }, 1);
+
+      toggleCollapseTag.classList = "accordion-toggle collapsing";
+
+      setTimeout(() => {
+        toggleCollapseTag.classList = "accordion-toggle collapse open";
+      }, 500);
+    } else {
+      toggleCollapseTag.classList = "accordion-toggle collapsing";
+      setTimeout(() => {
+        toggleCollapseTag.classList = "accordion-toggle collapse";
+      }, 1);
+
+      setTimeout(() => {
+        toggleCollapseTag.classList = "accordion-toggle collapse";
+        toggleCollapseTag.style.height = "";
+      });
+    }
+
+    el.classList.toggle("open");
+    // toggleCollapseTag.classList.toggle("open");
+  });
+});
